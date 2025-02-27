02ec51242d6e171a1caf33f57752c4f3df494976
jmlear <jmlear1110@gmail.com>
2025-02-27 06:31:17 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-12 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
