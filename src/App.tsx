f980ce7ae7ced6feaf663fe15977c2535c46a5a6
jmlear <jmlear1110@gmail.com>
2025-04-11 11:41:05 -0700

    Refined and added LinkedIn and GitHub links

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,10 @@ function App() {
 
   return (
     <>
-      <div className="h-1/2 lg:h-screen bg-gradient-to-t from-blue-200 relative">
+      <div className="h-screen bg-gradient-to-t from-blue-200 relative">
         <nav className='w-full fixed top-0'>
-          <div className='container mx-auto py-5 flex justify-between items-center text-gray-600'>
+          <div className='container mx-auto p-5 flex justify-between items-center text-gray-600'>
             <img className='w-10 h-10 md:w-20 md:h-20 object-cover rounded-full' src={JoelAvatar} />
-            <ul className='hidden md:flex space-x-4 uppercase'>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
             {/* Hamburger menu for small screens */}
             <div ref={hamburgerMenuButton} onClick={toggleHamburgerMenu} className='space-y-1 md:hidden cursor-pointer z-1'>
               <div className="w-6 h-0.5 bg-black"></div>
@@ -33,9 +29,8 @@ function App() {
             </div>
             {/* Opened hamburger menu */}
             <ul ref={hamburgerMenu} className='hidden md:hidden bg-indigo-900 absolute left-0 top-0 w-full p-10 rounded-b-3xl space-y-10 text-white text-center uppercase'>
-              <li>Services</li>
-              <li>Projects</li>
-              <li>Blog</li>
+              <li><a href="https://www.linkedin.com/in/joel-lear/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://github.com/jmlear" target="_blank">GitHub</a></li>
             </ul>
           </div>
         </nav>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,29 +2,38 @@
 export default function HomePage() {
   return (
     <>
-      <div className="h-full flex items-center justify-center flex-col">
-        <h1>Lead Software Developer</h1>
-        <p>Experienced, Motivated, Hard working</p>
-      </div>
-      {/* Circle Background */}
-      <div className="hidden lg:block absolute bottom-0 right-0 left-0 top-0 my-auto mx-auto w-96 h-96 bg-blue-900 rounded-full -z-10"></div>
       {/* Floating Left side text */}
-      <div className="absolute top-1/3 left-5 text-xl sm:left-10 sm:text-4xl lg:left-5 xl:left-48 font-bold">
+      <div className="static flex flex-col justify-center text-center h-1/2 md:text-left md:h-auto md:absolute md:top-1/3 md:left-5 text-xl sm:text-4xl lg:left-5 xl:left-48 font-bold">
         <span className="text-gray-600">Joel</span>
         <p className="text-red-500">Developer</p>
       </div>
-      {/* Text bubble on left */}
-      <div className="hidden p-5 lg:flex flex-col gap-5 rounded-md shadow-lg absolute top-0 bottom-0 m-auto right-10 bg-white h-fit w-1/3">
+      {/* Make div using tailwind css that spans the width of the screen but the text inside stays within the middle 1/3 of the screen and wraps as the screen gets smaller */}
+      {/* <div className="w-40 h-40 md:w-60 md:h-60 lg:w-80 lg:h-80 bg-blue-900 rounded-full"></div> */}
+      <div className="h-1/2 md:h-full w-full flex items-center justify-center flex-col gap-5 text-center">
+        <div className="flex flex-col w-60 lg:w-80">
+          <h1>Lead Software Developer</h1>
+          <p>Experienced, Motivated, Hard working</p>
+        </div>
+      </div>
+      {/* Circle Background */}
+      <div className="hidden md:block absolute bottom-0 right-0 left-0 top-0 my-auto mx-auto w-96 h-96 bg-blue-900 rounded-full -z-10"></div>
+      {/* Text bubble on right */}
+      <div className="hidden p-5 md:flex flex-col gap-5 rounded-md shadow-lg absolute top-0 bottom-0 m-auto right-10 bg-white h-fit w-1/3">
         <h1 className="text-4xl font-bold text-blue-900">Hi I'm Joel</h1>
-        <p className="text-gray-400">I am results-driven with extensive experience in modernizing legacy systems and integrating advanced frameworks.<br/><br/>I am a leader of cross-functional teams and I utilize automation to optimize workflows.<br/><br/>I have a strong problem-solving mindset with a commitment to clean, maintainable code.</p>
-        <a href="#contact" className="bg-blue-500 text-white text-xl px-3 py-2 w-fit rounded-md font-semibold">Contact Me</a>
+        <p className="text-gray-400">I am results-driven with extensive experience in modernizing legacy systems and integrating advanced frameworks.<br /><br />I am a leader of cross-functional teams and I utilize automation to optimize workflows.<br /><br />I have a strong problem-solving mindset with a commitment to clean, maintainable code.</p>
+        {/* Make links to LinkedIn, GitHub that open in new tab */}
+        {/* Make a container for the two link buttons that puts them in one row */}
+        <div className="flex gap-5">
+          <a href="https://www.linkedin.com/in/joel-lear/" target="_blank" rel="noopener noreferrer" className="bg-blue-500 text-white text-xl px-3 py-2 w-fit rounded-md font-semibold">LinkedIn</a>
+          <a href="https://github.com/jmlear" target="_blank" className="bg-blue-500 text-white text-xl px-3 py-2 w-fit rounded-md font-semibold">GitHub</a>
+        </div>
       </div>
       {/* About me section */}
-      <div className="px-10">
+      {/* <div className="px-10">
         <div className="container mx-auto py-40 flex flex-col-reverse lg:flex-row items-center gap-20">
-          {/* <div className="relative"> */}
+          
           <div className="w-40 h-40 bg-blue-900 rounded-full"></div>
-          {/* </div> */}
+          
           <div className="my-auto flex flex-col gap-3">
             <h1 className="font-bold text-blue-900">About Me</h1>
             <h1 className="text-3xl font-medium">Better Design</h1>
@@ -44,7 +53,7 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
